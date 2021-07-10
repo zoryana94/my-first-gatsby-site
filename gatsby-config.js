28c6f4f20004611bfa6d9383eb542429fdb78b8e
Zoryana Vakh <zoryana@leanconvert.com>
2021-07-10 15:34:59 +0300

    Add the query

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,17 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: 'https://www.yourdomain.tld',
-    title: 'My First Gatsby Site',
+    title: 'My First Gatsby Site'
   },
   plugins: [
     'gatsby-plugin-gatsby-cloud',
     'gatsby-plugin-image',
-    'gatsby-plugin-sharp'
-  ],
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    }
+  ]
 };
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,30 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
+import { Link, useStaticQuery, graphql } from 'gatsby';
 
 import * as styles from './layout.module.css';
 
 const Layout = ({ pageTitle, children}) => {
+  const metaData = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
   return (
     <main className={styles.container}>
-      <title>{pageTitle}</title>
+      <title>{pageTitle} | {metaData.site.siteMetadata.title}</title>
+      <p className={styles.siteTitle}>{metaData.site.siteMetadata.title}</p>
       <nav>
         <ul className={styles.navLinks}>
           <li className={styles.navLinkItem}>
             <Link to="/">Home</Link>
+          </li>
+          <li className={styles.navLinkItem}>
+            <Link to="/blog">Blog</Link>
           </li>
           <li className={styles.navLinkItem}>
             <Link to="/about">About</Link>
